8a513e39179b48c3c4cf9a91ab805f1a185a9ad6
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-03-26 08:35:59 -0300

    Diretivas e eventos

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -14,4 +14,31 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Setar a classe dinâmicamente, para usar no todo-item.component.html, como uma diretiva de atributo
+  // Dentro da variável let estou criando um objeto que tem relação com o css criado neste componente. Conforme abaixo
+  // .todo {
+  //   background: #f4f4f4;
+  //   padding: 10px;
+  //   border-bottom: 1px #ccc dotted;
+  // }
+  
+  // .is-complete {
+  //   text-decoration: line-through;
+  // }
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed // precisei do '' porque tem hífen no nome da classe 
+    }
+    return classes;
+  }
+
+  onToggle(todo) {
+    // console.log('toggle');
+    todo.completed = !todo.completed; // usando a negação p/ ficar mudando de completo para nâo completo
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
