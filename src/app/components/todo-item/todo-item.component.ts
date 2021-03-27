0d79f190881b91539e61628b648a3068f66298e9
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2021-03-27 16:42:13 -0300

    Adicionado o serviço de Toggle

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../service/todo.service'
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
   }
@@ -35,7 +36,13 @@ export class TodoItemComponent implements OnInit {
 
   onToggle(todo) {
     // console.log('toggle');
+    // UI na tela
     todo.completed = !todo.completed; // usando a negação p/ ficar mudando de completo para nâo completo
+    
+    // chamada do service
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    })
   }
 
   onDelete(todo) {
--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -5,10 +5,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 // Para fazer esta importação, antes eu preciso fazer a importação do import { HttpClientModule } from '@angular/common/http' e associar nos imports
 // no app.module.ts
 
-import { from, Observable } from 'rxjs'
+import { Observable } from 'rxjs'
 // Esta importação é necessária para que façamos a chamada do API
 
 import { Todo } from '../models/Todo'
+
+
+const httpOptions =  {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +34,15 @@ export class TodoService {
     // utilizar o .get para fazer o request, usando o http que eu injetei no construtor
     // o meu get retornará um array de Todo, por isso <Todo[]>. Necessário importar o Todo
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Aqui no caso do put, estou recebendo o todo no método, como parâmetro
+  // put também vai retornar um Observable, por isso terei que colocar no retorno
+  toggleCompleted(todo: Todo):Observable<any> { // any, porque o retorno não é do tipo Todo (poderia criar um tipo e usá-lo)
+    const url = `${this.todosUrl}/${todo.id}`; // montando a URL e passando o ID p/ poder fazer o put (update)
+    return this.http.put(url, todo, httpOptions); // httpOptions é para incluir o Header do ContentType
+    // @param url — The endpoint URL.
+    // @param body — The resources to add/update.
+    // @param options — HTTP options
   }
 }
